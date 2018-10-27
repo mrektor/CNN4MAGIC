a32fb8ef86e9bf189b616c4eab72fce3c9103ca7
Sektor <mr.ektor@gmail.com>
2018-10-27 17:56:10 +0200

    test with no th on energy

--- CNN_Models/EnergyRegressor/energy_regressor.py
+++ CNN_Models/EnergyRegressor/energy_regressor.py
@@ -5,7 +5,7 @@ from CNN_Models.EnergyRegressor.magic_inception import magic_inception
 from utils import *
 
 # % Data Loading
-x_train, y_train, x_test, y_test, input_shape = load_magic_data(logx=False, energy_th=2)
+x_train, y_train, x_test, y_test, input_shape = load_magic_data(logx=False, energy_th=0)
 
 model_reg = magic_inception(input_shape=input_shape, num_filters_first_conv=132, dropout=0, num_classes=1)
 
@@ -20,7 +20,7 @@ reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.4,
                               patience=4, min_lr=0.000005)
 
 result = model_reg.fit(x_train, y_train,
-                       batch_size=512,
+                       batch_size=450,
                        epochs=100,
                        verbose=1,
                        validation_data=(x_test, y_test),
@@ -30,8 +30,10 @@ result = model_reg.fit(x_train, y_train,
 
 
 y_pred = model_reg.predict(x_test)
+
+plot_hist2D(y_test, y_pred, net_name=net_name, num_bins=50)
+
 plot_gaussian_error(y_test, y_pred, net_name=net_name + '_10bin', num_bins=10)
 # plot_gaussian_error(y_test, y_pred, net_name=net_name + '_20bin', num_bins=20)
 
 # %
-plot_hist2D(y_test, y_pred, net_name=net_name, num_bins=50)
