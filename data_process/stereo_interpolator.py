ab0dc7563ea1ac10b47d517b84d1135f8059ecc8
Sektor <mr.ektor@gmail.com>
2018-11-02 16:00:02 +0100

    added directory automatic indexing

--- data_process/stereo_interpolator.py
+++ data_process/stereo_interpolator.py
@@ -20,8 +20,12 @@ filenames = [filenameM1, filenameM2]
 
 
 def stereo_interp_from_txt(filenames):
+
     filenameM1 = filenames[0]
     filenameM2 = filenames[1]
+
+    if filenameM1[-26:-7] != filenameM2[-26:-7]:
+        print('Ostia! filename are different: ', filenameM1, filenameM2)
 
     m1 = pd.read_csv(filenameM1, sep=' ', header=None)
     m2 = pd.read_csv(filenameM2, sep=' ', header=None)
@@ -43,12 +47,24 @@ def stereo_interp_from_txt(filenames):
     m2_interp = [interpolator.interpolate(instance, remove_last=False, plot=False) for instance in pixel_m2]
 
     result = {'trigger': trigger_values,
+              'energy': energy,
               'positionM1': position_1, 'positionM2': position_2,
               'M1_interp': m1_interp, 'M2_interp': m2_interp}
     print(filenameM1[-26:-7])
     with open('/data/mariotti_data/CNN4MAGIC/pickle_data/processed/result_' + filenameM1[-26:-7] + '.pkl',
               'wb') as f:
         pickle.dump(result, f, protocol=4)
+
+
+# %%
+import glob
+
+file1 = glob.glob('/data/mariotti_data/CNN4MAGIC/dataset/MC/Energy_SrcPosCam/M1/GA_M1_*.txt')
+file2 = glob.glob('/data/mariotti_data/CNN4MAGIC/dataset/MC/Energy_SrcPosCam/M1/GA_M1_*.txt')
+
+list_of_files = np.array([file1, file2]).T.tolist()
+
+# %%
 
 
 stereo_interp_from_txt(filenames)
