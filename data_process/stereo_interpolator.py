b322fc6d1950b849ad29ba2f2626605334124f81
Sektor <mr.ektor@gmail.com>
2018-11-02 15:31:35 +0100

    a step closer

--- data_process/stereo_interpolator.py
+++ data_process/stereo_interpolator.py
@@ -5,6 +5,8 @@ from pyspark import SparkConf, SparkContext
 # %%
 config = SparkConf().setAppName('Homework 2').setMaster('local[*]')
 sc = SparkContext(conf=config)
+import matplotlib.pyplot as plt
+from InterpolateMagic import InterpolateMagic
 
 # %%
 m1 = pd.read_csv('/data/mariotti_data/CNN4MAGIC/dataset/MC/Energy_SrcPosCam/M1/GA_M1_za05to35_8_821318_Y_w0.txt',
@@ -13,18 +15,12 @@ m1 = pd.read_csv('/data/mariotti_data/CNN4MAGIC/dataset/MC/Energy_SrcPosCam/M1/G
 m2 = pd.read_csv('/data/mariotti_data/CNN4MAGIC/dataset/MC/Energy_SrcPosCam/M2/GA_M2_za05to35_8_821318_Y_w0.txt',
                  sep=' ', header=None)
 
-# %%
+
 trigger1 = np.array(m1.iloc[:, 0])
 trigger2 = np.array(m2.iloc[:, 0])
 
 m1_idx_mask = np.isin(trigger1, trigger2)
-# m2_idx_mask = np.isin(trigger2, trigger1)
 
-m1_both_event = m1[m1_idx_mask, :]
-# m2_both_event = m2[m2_idx_mask, :] # m1 and m2 are equal
-
-
-# %%
 trigger_values = m1.iloc[m1_idx_mask, 0].values
 position_1 = m1.iloc[m1_idx_mask, 2:4].values
 position_2 = m2.iloc[m1_idx_mask, 2:4].values
@@ -32,27 +28,24 @@ energy = m1.iloc[m1_idx_mask, 1].values
 pixel_m1 = m1.iloc[m1_idx_mask, 2 + 2:1041 + 2].values
 pixel_m2 = m2.iloc[m1_idx_mask, 2 + 2:1041 + 2].values
 
-# %%
-import matplotlib.pyplot as plt
-from InterpolateMagic import InterpolateMagic
-
-# import data_process.InterpolateMagic
-
-# %%
 interpolator = InterpolateMagic(15)
-idx = 3
-a = interpolator.interpolate(pixel_m1[idx], remove_last=False, plot=False)  # Test
-b = interpolator.interpolate(pixel_m2[idx], remove_last=False, plot=False)
+m1_interp = [interpolator.interpolate(instance, remove_last=False, plot=False) for instance in pixel_m1]
+m2_interp = [interpolator.interpolate(instance, remove_last=False, plot=False) for instance in pixel_m2]
 
+# %% Test
+print(len(m1_interp))
+# %%
+
+idx = 3
+
+#Plot
 plt.figure()
 plt.subplot(1, 2, 1)
-plt.imshow(a)
+plt.imshow(m1_interp[idx])
 plt.title('M1')
-# plt.colorbar()
 
 plt.subplot(1, 2, 2)
-plt.imshow(b)
-# plt.colorbar()
+plt.imshow(m2_interp[idx])
 plt.title('M2')
 plt.tight_layout()
-plt.savefig('test3.png')
+plt.savefig('test.png')
