28a9c23ae4bc2ea0053167c10756e327a736ae75
Sektor <mr.ektor@gmail.com>
2018-11-06 11:50:08 +0100

    Fixed last bug, now it works like a charm. Multiprocessing Done.
    now need to add time signal

--- data_process/stereo_interpolator.py
+++ data_process/stereo_interpolator.py
@@ -19,8 +19,14 @@ def stereo_interp_from_txt(filenames):
     m1 = pd.read_csv(filenameM1, sep=' ', header=None)
     m2 = pd.read_csv(filenameM2, sep=' ', header=None)
 
-    trigger1 = np.array(m1.iloc[:, 0])
-    trigger2 = np.array(m2.iloc[:, 0])
+    # put int trigger1 the values of the trigger of the txt that have the highest number of simulated events.
+    # Not really elegant, but it should work.
+    if m1.iloc[:, 0].shape[0] < m2.iloc[:, 0].shape[0]:
+        trigger1 = np.array(m1.iloc[:, 0])
+        trigger2 = np.array(m2.iloc[:, 0])
+    else:
+        trigger2 = np.array(m1.iloc[:, 0])
+        trigger1 = np.array(m2.iloc[:, 0])
 
     m1_idx_mask = np.isin(trigger1, trigger2)  # TODO: check the smallest then do the isin accordingly
 
@@ -40,12 +46,17 @@ def stereo_interp_from_txt(filenames):
               'positionM1': position_1, 'positionM2': position_2,
               'M1_interp': m1_interp, 'M2_interp': m2_interp}
     print(filenameM1[-26:-7])
-    with open('/data/mariotti_data/CNN4MAGIC/pickle_data/processed/result_' + filenameM1[-26:-7] + '.pkl',
+    with open('/data2T/mariotti_data_2/energy_interp_stereo/result_' + filenameM1[-26:-7] + '.pkl',
               'wb') as f:
         pickle.dump(result, f, protocol=4)
 
 
 # %%
+# Load all the filenames
+fileM1 = glob.glob('/data/mariotti_data/CNN4MAGIC/dataset/MC/Energy_SrcPosCam/M1/GA_M1_*.txt')
+fileM2 = glob.glob('/data/mariotti_data/CNN4MAGIC/dataset/MC/Energy_SrcPosCam/M2/GA_M2_*.txt')
+
+
 def get_pair_match(a, b):
     result = []
     for i in a:
@@ -54,31 +65,8 @@ def get_pair_match(a, b):
                 result.append((i, j))
     return result
 
-# Load all the filenames
-fileM1 = glob.glob('/data/mariotti_data/CNN4MAGIC/dataset/MC/Energy_SrcPosCam/M1/GA_M1_*.txt')
-fileM2 = glob.glob('/data/mariotti_data/CNN4MAGIC/dataset/MC/Energy_SrcPosCam/M2/GA_M2_*.txt')
-
-# %% check the same number of simulation
-# nameM1 = np.array([fileM1[i][-26:-7] for i in range(len(fileM1))])
-# nameM2 = np.array([fileM1[i][-26:-7] for i in range(len(fileM2))])
-# name_mask1 = np.isin(nameM1, nameM2)
-# name_mask2 = np.isin(nameM2, nameM1)
 
 mFull = get_pair_match(fileM1, fileM2)
-
-# %%
-# m1 = np.array(fileM1)[name_mask1]
-# m2 = np.array(fileM2)[name_mask2]
-# m1 = np.sort(m1)
-# m2 = np.sort(m2)
-# mFull = [(m1[i], m2[i]) for i in range(len(m1))]
-# %%
-# a = np.where(nameM1 == nameM2, m1, m2)
-# %%
-# print(mFull[10])
-# %%
-# Each line here is a couple of filenames of the same simulation
-# list_of_files = np.array([np.sort(fileM1)[name_mask1], np.sort(fileM2)[name_mask2]]).T.tolist()
 
 # %%
 # Start the parallel computing
