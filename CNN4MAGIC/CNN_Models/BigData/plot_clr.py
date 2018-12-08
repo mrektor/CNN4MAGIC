b7a4a07259fa4ec4c6db79715a5228bb781683c6
Sektor <mr.ektor@gmail.com>
2018-12-08 22:38:53 +0100

    CLR finder tailored on single-SE-Denset

--- CNN4MAGIC/CNN_Models/BigData/plot_clr.py
+++ CNN4MAGIC/CNN_Models/BigData/plot_clr.py
@@ -2,17 +2,19 @@ import matplotlib.pyplot as plt
 from keras.optimizers import SGD
 
 from CNN4MAGIC.CNN_Models.BigData.clr import OneCycleLR
+from CNN4MAGIC.CNN_Models.BigData.loader import load_data_append
+from CNN4MAGIC.CNN_Models.BigData.stereo_models import single_DenseNet
 
 plt.style.use('seaborn-white')
 
 # Constants
-NUM_SAMPLES = 2000
-NUM_EPOCHS = 100
-BATCH_SIZE = 500
-MAX_LR = 0.1
+NUM_EPOCHS = 3
+BATCH_SIZE = 64
+MAX_LR = 0.5
 
 # Data
 m1_tr, m2_tr, energy_tr = load_data_append('train', prune=True)
+NUM_SAMPLES = energy_tr.shape[0]
 
 # Model
 model = single_DenseNet()
@@ -23,7 +25,7 @@ clr_triangular = OneCycleLR(NUM_SAMPLES, NUM_EPOCHS, BATCH_SIZE, MAX_LR,
 model.compile(optimizer=SGD(0.1), loss='binary_crossentropy', metrics=['accuracy'])
 
 model.fit({'m1': m1_tr, 'm2': m2_tr}, energy_tr, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS, callbacks=[clr_triangular],
-          verbose=0)
+          verbose=1)
 
 print("LR Range : ", min(clr_triangular.history['lr']), max(clr_triangular.history['lr']))
 print("Momentum Range : ", min(clr_triangular.history['momentum']), max(clr_triangular.history['momentum']))
@@ -32,14 +34,14 @@ plt.xlabel('Training Iterations')
 plt.ylabel('Learning Rate')
 plt.title("CLR")
 plt.plot(clr_triangular.history['lr'])
-plt.savefig(net_name + '_lr.png')
-plt.savefig(net_name + '_lr.eps')
+plt.savefig('/data/mariotti_data/CNN4MAGIC/CNN_Models/BigData/learning_rates_pics/' + net_name + '_lr.png')
+plt.savefig('/data/mariotti_data/CNN4MAGIC/CNN_Models/BigData/learning_rates_pics/' + net_name + '_lr.eps')
 plt.show()
 
 plt.xlabel('Training Iterations')
 plt.ylabel('Momentum')
 plt.title("CLR")
 plt.plot(clr_triangular.history['momentum'])
-plt.savefig(net_name + '_mom.png')
-plt.savefig(net_name + '_mom.eps')
+plt.savefig('/data/mariotti_data/CNN4MAGIC/CNN_Models/BigData/learning_rates_pics' + net_name + '_mom.png')
+plt.savefig('/data/mariotti_data/CNN4MAGIC/CNN_Models/BigData/learning_rates_pics' + net_name + '_mom.eps')
 plt.show()
