f8b3f692043a42f62d51ba50f36d9b32621c93ad
Sektor <mr.ektor@gmail.com>
2018-12-14 17:00:25 +0100

    no-Impact

--- CNN4MAGIC/CNN_Models/BigData/evaluate_net.py
+++ CNN4MAGIC/CNN_Models/BigData/evaluate_net.py
@@ -4,18 +4,20 @@ from keras.models import load_model
 from CNN4MAGIC.CNN_Models.BigData.loader import load_data_append
 from CNN4MAGIC.CNN_Models.BigData.utils import plot_hist2D, plot_gaussian_error
 
-net_name = 'single_DenseNet_25_3_doubleDense'
+net_name = 'single_DenseNet_25_3_doubleDense-noImpact'
 
 path = '/data/mariotti_data/CNN4MAGIC/CNN_Models/BigData/checkpoints/' + net_name + '.hdf5'
 print('Loading model ' + net_name + '...')
 model = load_model(path)
-m1_te, m2_te, energy_te = load_data_append('test', prune=True)
+m1_te, m2_te, energy_te = load_data_append('test', prune=True, leak=True)
 y_test = np.log10(energy_te)
 
 print('Making Predictions...')
 y_pred = model.predict({'m1': m1_te, 'm2': m2_te})
 
 # %%
+net_name = 'single_DenseNet_25_3_doubleDense_noImpactCutTrain-YesImpactCutTest'
+
 print('Plotting stuff...')
 plot_hist2D(y_test, y_pred,
             fig_folder='/data/mariotti_data/CNN4MAGIC/CNN_Models/BigData/pics/',
