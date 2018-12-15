10331d2174c9f418440de13a4ccc53ebef8f5128
Sektor <mr.ektor@gmail.com>
2018-12-15 15:35:31 +0100

    Evaluator script

---
+++ CNN4MAGIC/CNN_Models/SeparationStereo/separation_evaluate_net.py
@@ -0,0 +1,20 @@
+from keras.models import load_model
+
+from CNN4MAGIC.CNN_Models.SeparationStereo.utils import load_separation_data, plot_confusion_matrix, plot_gammaness
+
+net_name = 'single_DenseNet_25_3_doubleDense'
+
+path = '/data/mariotti_data/CNN4MAGIC/CNN_Models/SeparationStereo/checkpoints/' + net_name + '.hdf5'
+print('Loading model ' + net_name + '...')
+model = load_model(path)
+m1_te, m2_te, y_true = load_separation_data('test')
+
+print('Making Predictions...')
+y_pred = model.predict({'m1': m1_te, 'm2': m2_te})
+
+print('Plotting confusion matrix...')
+plot_confusion_matrix(y_pred, y_true, 2, net_name=net_name)
+print('Plotting gammaness...')
+plot_gammaness(y_true, y_true, net_name=net_name)
+
+print('All done')
