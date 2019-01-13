735a921c0e81c18a3a389788faabffcee6ec8c54
Sektor <mr.ektor@gmail.com>
2019-01-14 00:46:59 +0100

    Tested angular resolution, everything works.

--- CNN4MAGIC/Generator/evaluat_position_generator.py
+++ CNN4MAGIC/Generator/evaluat_position_generator.py
@@ -33,37 +33,14 @@ print(y_pred[:5], position_te_limato[:5])
 # %%
 
 
-from CNN4MAGIC.CNN_Models.BigData.utils import compute_theta, bin_data_mask
+from CNN4MAGIC.CNN_Models.BigData.utils import plot_angular_resolution
 
 # %%
 train_gn, val_gn, test_gn, energy_te = load_data_generators(batch_size=BATCH_SIZE, want_energy=True)
 # %%
 energy_te = np.array(energy_te)
 energy_te_limato = energy_te[:len(test_gn) * BATCH_SIZE]
-
-
 # %%
 
-
 # %%
-def plot_angular_resolution(position_true, position_prediction, energy_true,
-                            fig_folder='/data/code/CNN4MAGIC/Generator/position_pic'):
-    binned_values, bins, bins_masks = bin_data_mask(energy_true, 11)
-    resolutions = []
-    bin_medians = []
-    for i, mask in enumerate(bins_masks):
-        bin_pos = position_true[mask]
-        bin_pred_pos = position_prediction[mask]
-        bin_value = np.sqrt(bins[i] * bins[i + 1])
-        res = compute_theta(bin_pos, bin_pred_pos, plot=False)
-        resolutions.append(res)
-        bin_medians.append(bin_value)
-
-        plt.figure()
-        plt.plot(bin_medians, resolutions)
-        plt.xlabel('Energy')
-        plt.ylabel('Angular Resolution')
-        plt.title('Angular Resolution of ' + net_name)
-        plt.grid()
-        plt.savefig(fig_folder + '/angular_resolution' + net_name + '.png')
-        plt.show()
+plot_angular_resolution(position_te_limato, y_pred, energy_te_limato, net_name=net_name)
