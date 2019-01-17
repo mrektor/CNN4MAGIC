e58a2a636474615f044da1f78a8622d3f2a57a49
Sektor <mr.ektor@gmail.com>
2019-01-17 19:04:22 +0100

    added model evaluation on the fly

--- energy_snapshot.py
+++ energy_snapshot.py
@@ -1,17 +1,20 @@
+import numpy as np
+
 from CNN4MAGIC.CNN_Models.BigData.snapshot import SnapshotCallbackBuilder
+from CNN4MAGIC.CNN_Models.BigData.utils import plot_hist2D, plot_gaussian_error
 from CNN4MAGIC.Generator.gen_util import load_data_generators
-from CNN4MAGIC.Generator.models import MobileNetV2_2dense_energy
+from CNN4MAGIC.Generator.models import single_DenseNet_25_3_doubleDense
 
 M = 5  # number of snapshots
 nb_epoch = T = 10  # number of epochs
 alpha_zero = 0.03  # initial learning rate
-net_name = 'MobileNetV2_2dense_energy_snap'
+net_name = 'single_DenseNet_25_3_doubleDense_snap'
 
 snapshot = SnapshotCallbackBuilder(T, M, alpha_zero)
 callbacks = snapshot.get_callbacks(model_prefix=net_name)
 
 print('Loading the Neural Network...')
-model = MobileNetV2_2dense_energy()
+model = single_DenseNet_25_3_doubleDense()
 model.compile(optimizer='sgd', loss='mse')
 model.summary()
 print('Model Loaded.')
@@ -32,3 +35,16 @@ result = model.fit_generator(generator=train_gn,
 
 # %%
 print('Training done.')
+
+print('Making predictions on test set...')
+y_pred = model.predict_generator(generator=test_gn, verbose=1, use_multiprocessing=True, workers=24)
+
+energy_te = np.array(energy_te)
+energy_te_limato = energy_te[:len(test_gn) * BATCH_SIZE]
+
+plot_hist2D(energy_te_limato, y_pred, net_name, fig_folder='/data/code/CNN4MAGIC/Generator/energy_pic/',
+            num_bins=100)
+# %%
+plot_gaussian_error(energy_te_limato, y_pred, net_name=net_name,
+                    fig_folder='/data/code/CNN4MAGIC/Generator/energy_pic/')
+print('All done.')
