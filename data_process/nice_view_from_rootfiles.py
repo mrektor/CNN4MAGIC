6cf28795225cf6f2f22b1b0505a95f8cb99498f9
Sektor <mr.ektor@gmail.com>
2019-02-19 17:22:14 +0100

    visualizzazione ricostruzione energia con rete vecchia. Codice brutto ma hey, funziona!

--- data_process/nice_view_from_rootfiles.py
+++ data_process/nice_view_from_rootfiles.py
@@ -1,6 +1,7 @@
 from glob import glob
 
 import matplotlib.pyplot as plt
+import numpy as np
 from ctapipe.instrument import CameraGeometry
 from ctapipe.visualization import CameraDisplay
 
@@ -60,33 +61,92 @@ def magic_plot_time(filename, idx):
 
 
 # %%
-
-
-# %%
 m1, m2 = filenames[0]
 print(m1, m2)
 res = stereo_interp_from_root((m1, m2))
 
 # %%
-print(m1, m1[55:70])
-# %%
-files_m1_npy = glob(f'/ssdraptor/magic_data/data_processed/diffuse/*{m1[55:69]}*.npy')
-print(len(files_m1_npy))
-import numpy as np
 
-npy_files = [np.load(file) for file in files_m1_npy]
+i2 = res['M2_interp']
+i1 = res['M1_interp']
+print(i1.shape, i2.shape)
+i12 = []
+for idx in range(i1.shape[0]):
+    evt1 = i1[idx]
+    evt2 = i2[idx]
 
-# %%
-print(res['M1_interp'].shape)
+    # print(evt1.shape, evt2.shape)
+    b = np.zeros((67, 68, 4))
+    b[:, :, 0] = evt1[0]
+    b[:, :, 1] = evt1[1]
+    b[:, :, 2] = evt2[0]
+    b[:, :, 3] = evt2[1]
+    i12.append(b)
+
+i12_vect = np.array(i12)
+print(i12_vect.shape)
 # %%
 from keras.models import load_model
 
 model = load_model('/home/emariott/deepmagic/CNN4MAGIC/Generator/checkpoints/MobileNetV2_4dense_position-big-2.hdf5')
+pos_pred = model.predict_on_batch(i12_vect)
+
 # %%
-single = npy_files[0]
-print(single[np.newaxis].shape)
+# pos_true = np.concatenate([res['src_X1'], res['src_Y1']])
+posx_true = res['src_X1'].values
+posy_true = res['src_Y1'].values
+
+posx_true_deg = posx_true * 0.00337
+posy_true_deg = posy_true * 0.00337
+
+pos_pred_deg = pos_pred * 0.00337
+
+theta2 = np.power(pos_pred_deg[:, 0] - posx_true_deg, 2) + np.power((pos_pred_deg[:, 1] - posy_true_deg), 2)
+print(theta2.shape)
+
+quali_theta_2_piccoli = theta2 < 0.01
+theta2_piccoli = theta2[theta2 < 0.01]
+
+secondo_bin_theta = (theta2 > 0.01) & (theta2 < 0.04)
+
+
 # %%
-pos_pred = model.predict_on_batch(single[np.newaxis])
+df, phe, time = read_from_root(m1)
+
+
+# %%
+def magic_plot_phe_pos(idx, pos_true_x, pos_true_y, pos_reco_x, pos_reco_y, highlight=False):
+    phe_to_view = phe.iloc[idx, :1039]
+    camera_MAGIC = CameraGeometry.from_name('MAGICCamMars')
+    plt.figure()
+    display = CameraDisplay(camera_MAGIC)
+
+    display.image = phe_to_view
+    display.add_colorbar()
+    plt.plot(pos_true_x * 0.001, pos_true_y * 0.001, 'xr', markersize=15)
+    plt.plot(pos_reco_x * 0.001, pos_reco_y * 0.001, 'xw', markersize=15)
+
+    if highlight:
+        display.highlight_pixels([i for i in range(len(phe_to_view.values))])
+    plt.title(f'MAGIC cam Phe signal of event {idx}')
+
+    plt.savefig(f'/home/emariott/deepmagic/data_process/visualisation_pics/pos_01_highlight/magic_camera_{idx}_phe.png')
+    plt.close()
+
+
+# %%
+def get_index_from_bool(bool_idx):
+    a = np.array([i for i in range(len(bool_idx))])[bool_idx]
+    return a
+
+
+# %%
+t001 = get_index_from_bool(quali_theta_2_piccoli)
+t004 = get_index_from_bool(secondo_bin_theta)
+from tqdm import tqdm
+
+for idx in tqdm(t004):
+    magic_plot_phe_pos(idx, posx_true[idx], posy_true[idx], pos_pred[idx, 0], pos_pred[idx, 1], highlight=True)
 
 
 # %%
@@ -139,15 +199,8 @@ plt.close()
 print(res.keys())
 # %%
 print(res['corsika_event_number_1'])
-print(sorted(files_m1_npy))
+# print(sorted(files_m1_npy))
 
-# %%
-
-i2 = res['M2_interp']
-i1 = res['M1_interp']
-print(i1.shape, i2.shape)
-i12 = np.concatenate((i1, i2), axis=1)
-print(i12.shape)
 
 # %%
 plt.figure()
@@ -157,7 +210,7 @@ plt.close()
 
 # %%
 magic_plot_phe(m2, 0)
-#%%
+# %%
 magic_plot_time(m1, idx_to_plot)
 magic_plot_time(m2, idx_to_plot)
 
