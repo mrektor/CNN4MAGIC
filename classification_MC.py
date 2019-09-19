eaf8c5ccc94f3c63af17e6534e6ea89c6bf9e611
Ettore <mr.ektor@gmail.com>
2019-09-19 15:15:20 +0200

    plots

--- classification_MC.py
+++ classification_MC.py
@@ -1,6 +1,6 @@
 import matplotlib
 
-matplotlib.use('TkAgg')
+# matplotlib.use('TkAgg')
 # from CNN4MAGIC.Generator.gen_util import load_generators_diffuse_point
 # from CNN4MAGIC.Generator.models import dummy_cnn
 # from CNN4MAGIC.Generator.training_util import snapshot_training
@@ -8,7 +8,7 @@ matplotlib.use('TkAgg')
 BATCH_SIZE = 512
 machine = 'towerino'
 
-# %% Load the data
+# % Load the data
 import pickle
 
 
@@ -22,7 +22,7 @@ def pickle_dump(filepath, object):
         pickle.dump(object, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-# %% Load complement
+# % Load complement
 folder_complement = '/ssdraptor/magic_data/classification_MC/complementary'
 diffuse_filenames_list, diffuse_labels, diffuse_energies, diffuse_positions = pickle_read(
     f'{folder_complement}/diffuse_complement.pkl')
@@ -34,10 +34,10 @@ protons_complement = pickle_read(f'{folder_complement}/protons_big_df_ID_Labels.
 
 protons_labels = protons_complement.set_index('ID').to_dict()['label']
 
-# %% Point to all files folder
+# % Point to all files folder
 npy_folder = '/ssdraptor/magic_data/classification_MC/all_npy'
 
-# %% Make Clean
+# % Make Clean
 golden_df_diffuse = diffuse_df[
     (diffuse_df['intensity_M1'] > 50) &
     (diffuse_df['intensity_M2'] > 50) &
@@ -61,7 +61,7 @@ else:
     ids_point = point_df['ID'].values
 
 ids_protons = protons_complement['ID'].values
-# %% tr-va-te split
+# % tr-va-te split
 
 num_protons = len(ids_protons)
 ids_protons_tr = ids_protons[:int(num_protons * 0.7)]
@@ -78,31 +78,31 @@ num_point = len(ids_point)
 ids_point_va = ids_point[:int(num_point * 0.2)]
 ids_point_te = ids_point[int(num_point * 0.2):int(num_point * 0.4)]
 
-# %% Define file list
+# % Define file list
 train_list_global = list(ids_protons_tr) + list(ids_diffuse_tr)
 validation_list_global = list(ids_protons_va) + list(ids_diffuse_va) + list(ids_point_va)
 test_list_global = list(ids_protons_test) + list(ids_diffuse_te) + list(ids_point_te)
 
-# %%
+# %
 print(len(ids_protons_tr), len(ids_diffuse_tr))
 print(len(ids_protons_va), len(ids_diffuse_va), len(ids_point_va))
 print(len(ids_protons_test), len(ids_diffuse_te), len(ids_point_te))
-# %% Define global label lookup dictionary
+# % Define global label lookup dictionary
 global_lookup_labels = dict()
 global_lookup_labels.update(point_labels)
 global_lookup_labels.update(diffuse_labels)
 global_lookup_labels.update(protons_labels)
 print(len(global_lookup_labels))
-# %% shuffle (might not be necessary)
+# % shuffle (might not be necessary)
 import random
 
 random.seed(42)
 random.shuffle(train_list_global)
 
-# %% define generators
+# % define generators
 from CNN4MAGIC.Generator.keras_generator import MAGIC_Generator
 
-# %%
+# %
 train_gn = MAGIC_Generator(list_IDs=train_list_global,
                            labels=global_lookup_labels,
                            separation=True,
@@ -121,22 +121,97 @@ val_gn = MAGIC_Generator(list_IDs=validation_list_global,
                          apply_log_to_raw=False,
                          include_time=True
                          )
-# %%
+# %
 from keras_radam import RAdam
 from CNN4MAGIC.Generator.models import MobileNetV2_separation
 
 model = MobileNetV2_separation()
 model.compile(optimizer=RAdam(), loss='binary_crossentropy', metrics=['accuracy'])
 model.summary()
-# %%
+# %
 result = model.fit_generator(generator=train_gn,
                              validation_data=val_gn,
-                             epochs=2,
+                             epochs=1,
                              verbose=1,
                              # callbacks=callbacks,
                              use_multiprocessing=False,
                              workers=8)
 
-# %%
-pred_on_val = model.evaluate_generator(val_gn, verbose=1, workers=8)
+# %
+hadron_test_gn = MAGIC_Generator(list_IDs=list(ids_protons_test),
+                                 labels=global_lookup_labels,
+                                 separation=True,
+                                 shuffle=False,
+                                 batch_size=BATCH_SIZE,
+                                 folder=npy_folder,
+                                 apply_log_to_raw=False,
+                                 include_time=True
+                                 )
 
+diffuse_test_gn = MAGIC_Generator(list_IDs=list(ids_diffuse_te),
+                                  labels=global_lookup_labels,
+                                  separation=True,
+                                  shuffle=False,
+                                  batch_size=BATCH_SIZE,
+                                  folder=npy_folder,
+                                  apply_log_to_raw=False,
+                                  include_time=True
+                                  )
+
+point_test_gn = MAGIC_Generator(list_IDs=list(ids_point_te),
+                                labels=global_lookup_labels,
+                                separation=True,
+                                shuffle=False,
+                                batch_size=BATCH_SIZE,
+                                folder=npy_folder,
+                                apply_log_to_raw=False,
+                                include_time=True
+                                )
+
+# %%
+prediction_hadron_test = model.predict_generator(hadron_test_gn, verbose=1, workers=8, max_queue_size=50)
+prediction_diffuse_test = model.predict_generator(diffuse_test_gn, verbose=1, workers=8, max_queue_size=50)
+prediction_point_test = model.predict_generator(point_test_gn, verbose=1, workers=8, max_queue_size=50)
+dump_folder = '/data4T/CNN4MAGIC/results/MC_classification/computed_data'
+pickle_dump(f'{dump_folder}/pred_hadr_te.pkl', prediction_hadron_test)
+pickle_dump(f'{dump_folder}/pred_diff_te.pkl', prediction_diffuse_test)
+pickle_dump(f'{dump_folder}/pred_point_te.pkl', prediction_point_test)
+model.save(f'{dump_folder}/one-epoch-MV2.h5')
+# %%
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(14, 8))
+plt.hist(prediction_hadron_test, label='Hadrons', bins=100, alpha=0.5, log=True)
+plt.hist(prediction_diffuse_test, label='Diffuse $\gamma$', bins=100, alpha=0.5, log=True)
+plt.hist(prediction_point_test, label='Point-Like $\gamma$', bins=100, alpha=0.5, log=True)
+plt.title('Distribution of Gammaness on Test Set (MC Classifcation)')
+plt.legend()
+plt.xlabel('Gammaness')
+plt.ylabel('Counts (Log scale)')
+plt.tight_layout()
+plt.savefig('/data4T/CNN4MAGIC/results/MC_classification/plots/one-epoch-MV2-gammaness.png')
+plt.close()
+# %%
+import numpy as np
+
+
+def efficiency(prob_vector, num_cuts=100):
+    total_number_events = len(prob_vector)
+    eff_list = []
+    gammaness_range = np.linspace(1e-9, 1-1e-9, num_cuts)
+    for gammaness_cut in gammaness_range:
+        num_selected = np.sum(prob_vector > gammaness_cut)
+        eff_list.append(num_selected / total_number_events)
+    return np.array(eff_list)
+
+point_eff = efficiency(prediction_point_test)
+hadron_eff = efficiency(prediction_hadron_test)
+q_factor = point_eff/np.sqrt(hadron_eff)
+
+plt.figure(figsize=(14, 8))
+plt.plot(np.linspace(1e-9, 1-1e-9, 100), q_factor, '-o')
+plt.ylabel('Q Factor')
+plt.xlabel('Gammaness')
+plt.title('Q Factor (MC classification)')
+plt.savefig('/data4T/CNN4MAGIC/results/MC_classification/plots/q_factor.png')
+plt.close()
